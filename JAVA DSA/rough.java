f8885dcf56bf1eccb94ddff3e52c26188290d823
Aztec331 <adityabuzz331@gmail.com>
2024-04-07 17:12:24 +0530

    DSA aur kya

--- JAVA DSA/rough.java
+++ JAVA DSA/rough.java
@@ -6,8 +6,21 @@ import java.util.regex.*;
 
 public class rough {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++){
+            arr[i]= scan.nextInt();
+        }
+
+        System.out.println(largestatleast2wice(arr);)
+}
+    public static int largestatleast2wice(int[] arr){
 
         
+
     }
+
 }
