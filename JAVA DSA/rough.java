e356cb7e1fe7237af28679da514e33ea87a74727
Aztec331 <adityabuzz331@gmail.com>
2024-04-07 10:34:13 +0530

    DSA aur kya

--- JAVA DSA/rough.java
+++ JAVA DSA/rough.java
@@ -1,13 +1,12 @@
-class rough{
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class rough {
     public static void main(String[] args) {
-        int a = 45;
-        float b = 56.7f;
-
-        System.out.println(addy(a, b));
-    }
-
-    public static float addy(int x, float y){
-        float additon = x+y;
-        return additon;
+        Scanner scan = new Scanner(System.in);
+        
     }
 }
