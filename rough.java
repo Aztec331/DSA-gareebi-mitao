fe909a9e1d8638481f2a6aa051990c413e8e276e
Aztec331 <adityabuzz331@gmail.com>
2024-08-26 07:28:07 +0530

    DSA aur kya

--- rough.java
+++ rough.java
@@ -9,7 +9,21 @@ import javax.sound.sampled.SourceDataLine;
 public class rough {
     public static void main(String[] args) {
 
-        System.out.println();
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int arr[] = new int[n];
+
+        for(int i=0; i<n; i++){
+            arr[i]= scan.nextInt();
+        }
+
+        //a variable to store sum
+        int sum=0;
+        for(int i=0; i<n; i++){
+            sum+=arr[i]
+        }
+
     }
     
 }
