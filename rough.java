de2b8e75d3f5740d199dba94f478669c7789bfeb
Aztec331 <adityabuzz331@gmail.com>
2024-08-29 20:32:57 +0530

    DSA aur kya

--- rough.java
+++ rough.java
@@ -6,7 +6,7 @@ import java.util.regex.*;
 
 public class rough {
     public static void main(String[] args) {
-        //Reverse part of array
+        //rotate array last to first k times 
 
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter size of array:");
@@ -19,10 +19,15 @@ public class rough {
             arr[i]= scan.nextInt();
         }
 
-        System.out.print("Enter index1:");
-        int x= scan.nextInt();
-        System.out.print("Enter index1:");
-        int y= scan.nextInt();
+        System.out.print("Enter value of k:");
+        int k = scan.nextInt();
+
+        k= k%n; // to avoid to many rotations to conserve time complexity
+
+        rotatelasttofirstbyk(arr,0,n-1);
+        rotatelasttofirstbyk(arr, 0, k-1);
+        rotatelasttofirstbyk(arr, k, n-1);
+
 
         for(int i=0; i<n; i++){
             System.out.print(arr[i]+ " ");
@@ -30,6 +35,22 @@ public class rough {
 
         scan.close();
 
+    }
+
+    public static void rotatelasttofirstbyk(int arr[],int s, int e){
+
+        int sp= s;
+        int ep= e;
+
+        while(sp<ep){
+
+            int temp = arr[sp];
+            arr[sp]= arr[ep];
+            arr[ep]= temp;
+
+            sp++;
+            ep--;
+        }
     }
 
 
