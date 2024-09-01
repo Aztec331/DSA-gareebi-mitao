d1fb6da60319536139306f21a5791e0fb0bd879c
Aztec331 <adityabuzz331@gmail.com>
2024-09-01 12:06:19 +0530

    DSA aur kya

--- array9_max_difference1.java
+++ array9_max_difference1.java
@@ -1,7 +1,48 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
 public class array9_max_difference1 {
     public static void main(String[] args) {
 
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter how many elements u want:");
+        int n = scan.nextInt();
+
+        int arr[] = new int[n];
+
+        for(int i=0; i<n; i++){
+            System.out.print("Enter array elements:");
+            arr[i]= scan.nextInt();
+        }
+
+        System.out.println(max_diff1(arr));
+
+        scan.close();
+
+    }
+
+    public static int max_diff1(int arr[]){
+
+        int max= Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+
+        return max-min;
+
+
     }
     
 }
+
+//TC 
---
+++ array9_max_difference2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class array9_max_difference2{
+    public static void main(String[] args) {
+
+        
+    }   
+    
+    public static int max_diff2(int arr[]){
+
+    }
+}
+
