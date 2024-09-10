9d7b76ef4646c0b1acbf1337f3df5f15aa70e041
Aztec331 <adityabuzz331@gmail.com>
2024-09-10 19:18:06 +0530

    DSA aur kya

--- array11_2darray_rowwise.java
+++ array11_2darray_rowwise.java
@@ -19,8 +19,8 @@ public class array11_2darray_rowwise{
         System.out.println();
         
         //for printing the elements
-        for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr[0].length; j++){
+        for(int i=0; i<arr[0].length; i++){
+            for(int j=0; j<arr.length; j++){
                 System.out.print(arr[i][j]+ " ");
             }
             System.out.println();
