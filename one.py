deac9df3680c89569894d53b22023745cad20369
Aztec331 <adityabuzz331@gmail.com>
2026-01-09 13:10:11 +0530

    DSA aur kya

---
+++ one.py
@@ -0,0 +1,7 @@
+# In a premier championship series of sports car racing,
+#  initially, the 1st car is ahead of the 2nd car by X metres. 
+# After that, in every second, the 1st car moves ahead by n1 metres 
+# and the 2nd car moves ahead by n2 metres (in the same direction).
+# Print the number of seconds after which the 2nd car crosses the 1st car.
+#  If it is impossible, print -1.
+
